0d783cedb66065cbecceb41fb535899d065ba60b
Florian Gilcher <florian.gilcher@asquera.de>
2014-11-01 14:01:56 -0700

    Remove name method, test as_slice

--- tests/key.rs
+++ tests/key.rs
@@ -16,15 +16,25 @@ impl Key for MyKey {
   fn from_u8(key: &[u8]) -> MyKey {
     MyKey { val: One }
   }
+
   fn as_slice<T>(self, f: |v: &[u8]| -> T) -> T {
     f("test".as_bytes())
   }
-  fn compare(&self, _other: &Key) -> Ordering {
+
+  fn compare(&self, _other: &MyKey) -> Ordering {
     Equal
   }
 }
 
 #[test]
 fn test() {
-  let key = from_u8("test".as_bytes());
+  let key: MyKey = from_u8("test".as_bytes());
+}
+
+#[test]
+fn test2() {
+  let key = MyKey { val: One };
+  key.as_slice(|k| {
+    assert_eq!(k, "test".as_bytes())
+  })
 }
